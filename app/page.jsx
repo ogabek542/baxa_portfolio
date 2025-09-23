82406cdd09201b80d39be7c3a278bc382370d238
ogabek542 <otaxonovo22222@gmail.com>
2025-09-23 18:05:13 +0500

    enlarge socila icons

--- app/page.jsx
+++ app/page.jsx
@@ -63,8 +63,8 @@ const Home = () => {
               </Button>
               <div className="mb-8 xl:mb-0 ">
                 <Socials
-                  containerStyles="flex gap-6 "
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  containerStyles="flex gap-5 "
+                  iconStyles="w-[45px] h-[45px] p-[0px] border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
                 />
               </div>
             </div>
